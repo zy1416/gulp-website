a7701633048d64947af384e311a165c9ea88db99
zhaoyue1 <zhaoyue1@ucfgroup.com>
2018-10-25 15:26:14 +0800

    对less增加支持

--- README.md
+++ README.md
@@ -1,1 +1,10 @@
-# gulp-website
+开发说明
+
+1. gulpfile.js 文件中配置了开发环境和生产编译环境。通过修改isProduction改变环境。
+
+   开发联调：isProduction设置为false.
+
+   生产编译：isProduction设置为true.
+
+   在运行default任务，会在不同的环境下去执行不同的任务列表。
+
--- gulpfile.js
+++ gulpfile.js
@@ -1,18 +1,49 @@
-var gulp = require('gulp')
-var uglify = require('gulp-uglify');//js压缩
-var minifyCSS = require('gulp-minify-css'); //css压缩
-var imagemin = require('gulp-imagemin');// //html压缩
-var babel = require('gulp-babel')
-var rev = require('gulp-rev');//- 对文件名加MD5后缀
-var revCollector = require('gulp-rev-collector'); //替换html中的文件名
-var clean = require('gulp-clean');//清空文件夹，避免文件冗余
-var runSequence = require('run-sequence'); //执行顺序，避免
-var autoprefixer = require("gulp-autoprefixer"); //增加css浏览器头
-var htmlmin = require('gulp-htmlmin'); //压缩html文件
-var concat = require('gulp-concat'); //文件合并
+const gulp = require('gulp')
+const uglify = require('gulp-uglify');//js压缩
+const minifyCSS = require('gulp-minify-css'); //css压缩
+const imagemin = require('gulp-imagemin');// //html压缩
+const babel = require('gulp-babel')
+const rev = require('gulp-rev');//- 对文件名加MD5后缀
+const revCollector = require('gulp-rev-collector'); //替换html中的文件名
+const clean = require('gulp-clean');//清空文件夹，避免文件冗余
+const runSequence = require('run-sequence'); //执行顺序，避免
+const autoprefixer = require("gulp-autoprefixer"); //增加css浏览器头
+const htmlmin = require('gulp-htmlmin'); //压缩html文件
+const concat = require('gulp-concat'); //文件合并
+const less = require('gulp-less');//less文件编译
+const connect = require('gulp-connect');//gulp 服务器
 
 
-var src_css = './trunk/css',
+
+/**********************************************
+ *     开发环境
+ **********************************************/
+const dev_less_src = './trunk/styles',
+    dev_css_src = './trunk/css';
+
+gulp.task('build-less', function () {
+    return gulp.src(dev_less_src + '/**/index.less')
+        .pipe(less())
+        .pipe(gulp.dest(dev_css_src))
+});
+
+gulp.task('watch-less', function () {
+   return gulp.watch('./trunk/styles/*.less', ['build-less']); // 注意，任务列表是个数组，即使只有一个元素。
+});
+
+gulp.task('server', function () {
+    connect.server({
+        root: "./trunk",
+        port: 3011,
+        livereload:true
+    });
+});
+
+
+/**********************************************
+ *     生产编译
+ **********************************************/
+const src_css = './trunk/css',
     dest_css = './dist/css',
     src_js = './trunk/js',
     dest_js = './dist/js',
@@ -47,7 +78,7 @@ gulp.task('build-css', function () {
             cascade: true, //是否美化属性值 默认：true 像这样：
             //-webkit-transform: rotate(45deg);
             //        transform: rotate(45deg);
-            remove:true //是否去掉不必要的前缀 默认：true
+            remove: true //是否去掉不必要的前缀 默认：true
         }))
         .pipe(minifyCSS())
         .pipe(rev())
@@ -60,14 +91,14 @@ gulp.task('build-css', function () {
 
 gulp.task('build-js', function () {
     //找到需要压缩的文件
-    return gulp.src(src_js+'/**/*.js')   ////  /**/  表示js目录下的任意层级的目录
-        //把ES6代码转成ES5代码
+    return gulp.src(src_js + '/**/*.js')   ////  /**/  表示js目录下的任意层级的目录
+    //把ES6代码转成ES5代码
         .pipe(babel())
         /////压缩文件
         .pipe(uglify())
         //- 文件名加MD5后缀
         .pipe(rev())
-         //另存压缩后文件
+        //另存压缩后文件
         .pipe(gulp.dest(dest_js))
         //- 生成一个rev-manifest.json
         .pipe(rev.manifest())
@@ -77,7 +108,7 @@ gulp.task('build-js', function () {
 
 
 gulp.task('build-html', function () {
-    return gulp.src(['rev/**/*.json', src_html+'/**/*.html']) //- 读取 rev-manifest.json 文件以及需要进行css名替换的文件
+    return gulp.src(['rev/**/*.json', src_html + '/**/*.html']) //- 读取 rev-manifest.json 文件以及需要进行css名替换的文件
         .pipe(revCollector())  //- 执行文件内css,js,img名的替换
         .pipe(htmlmin({
             removeComments: true,//清除HTML注释
@@ -93,12 +124,19 @@ gulp.task('build-html', function () {
 });
 
 
-gulp.task('default', function () {
-    runSequence(
-        'clean',
-        "build-img",
-        "build-css",
-        "build-js",
-        "build-html"
-    );
-});
+let isProduction = true;
+if (isProduction) {
+    gulp.task('default', function () {
+        runSequence(
+            'clean',
+            'build-less',
+            "build-img",
+            "build-css",
+            "build-js",
+            "build-html"
+        );
+    });
+} else {
+    gulp.task('default', ['build-less','watch-less','server'])
+}
+
